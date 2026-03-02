77c30205cd06eba6d1b1a17ec64ca852b11a3e2f
Paola <esquivelp.nancypaola@gmail.com>
2026-03-02 15:46:41 -0600

    Delete Backend/recursos-humanos-spring directory

--- Backend/recursos-humanos-spring/src/main/java/gm/rh/repositorio/EmpleadoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package gm.rh.repositorio;
-
-import gm.rh.modelo.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoRepositorio extends JpaRepository <Empleado,Integer> {
-}
